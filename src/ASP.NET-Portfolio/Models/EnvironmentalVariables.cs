c8ce859dd314682433a2f6b959dec65722b78349
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-08-25 10:41:42 -0700

    add Environmental Variable with token

---
+++ src/ASP.NET-Portfolio/Models/EnvironmentalVariables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Portfolio.Models
+{
+    public class EnvironmentalVariables
+    {
+        public static string AuthToken = "9d92ff8e294789354265f88707e5099769c7add1";
+    }
+}
